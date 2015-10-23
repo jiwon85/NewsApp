864910939dc99747aecb33d476ce5ee3492101ac
jiwon85 <jiwon.min85@gmail.com>
2015-10-23 18:49:01 -0500

    initial commit

---
+++ app/src/main/java/com/inf385t/jiwon_85/newsapp/CreateAccountActivity.java
@@ -0,0 +1,100 @@
+package com.inf385t.jiwon_85.newsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class CreateAccountActivity extends ActionBarActivity implements View.OnClickListener {
+
+
+    private EditText createUsernameText;
+    private EditText createPasswordText;
+    private EditText confirmPasswordText;
+    private EditText createEmailText;
+    private Button createButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        createUsernameText = (EditText) findViewById(R.id.createUsernameText);
+
+        createPasswordText = (EditText) findViewById(R.id.createPasswordText);
+        confirmPasswordText = (EditText) findViewById(R.id.confirmPasswordText);
+        createEmailText = (EditText) findViewById(R.id.createEmailText);
+        createButton = (Button) findViewById(R.id.createButton);
+        createButton.setOnClickListener(this);
+        createButton.setClickable(false);
+        Intent i = getIntent();
+        String username = i.getStringExtra("username"); //nullable!
+        if(username != null) {
+            createUsernameText.setText(username);
+        }
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override public void afterTextChanged(Editable s) {
+                if(createUsernameText.getText().length()!= 0
+                        && createPasswordText.getText().length()!= 0
+                        && confirmPasswordText.getText().length()!= 0
+                        && createEmailText.getText().length() != 0) {
+                    createButton.setClickable(true);
+                }
+                if(!createPasswordText.getText().toString()
+                        .equals(confirmPasswordText.getText().toString())) {
+                    //TODO: toast for passwords don't match
+
+                    createButton.setClickable(false);
+                }
+                if(!createEmailText.getText().toString().contains("@") ||
+                        !createEmailText.getText().toString().contains(".")){
+                    //TODO: toast for not a valid email
+                    createButton.setClickable(false);
+                }
+            }
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        };
+
+        createUsernameText.addTextChangedListener(textWatcher);
+        createPasswordText.addTextChangedListener(textWatcher);
+        confirmPasswordText.addTextChangedListener(textWatcher);
+        createEmailText.addTextChangedListener(textWatcher);
+
+
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.createButton) {
+            ParseUser user = new ParseUser();
+            user.setUsername(createUsernameText.getText().toString());
+            user.setPassword(createPasswordText.getText().toString());
+            user.setEmail(createEmailText.getText().toString());
+            user.signUpInBackground(new SignUpCallback() {
+                public void done(ParseException e) {
+                    if (e == null) {
+                        // Hooray! Let them use the app now.
+                        Toast t = Toast.makeText(getApplicationContext(), "success!", Toast.LENGTH_SHORT);
+                        t.show();
+                        //TODO: move to next screen
+                    } else {
+                        // Sign up didn't succeed. Look at the ParseException
+                        // to figure out what went wrong
+
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/inf385t/jiwon_85/newsapp/MainActivity.java
@@ -0,0 +1,106 @@
+package com.inf385t.jiwon_85.newsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button loginButton;
+    private Button createAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Parse.initialize(this, getString(R.string.parseappid), getString(R.string.parseclientkey));
+
+        usernameText = (EditText) findViewById(R.id.usernameText);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
+        loginButton.setClickable(false);
+        createAccountButton = (Button) findViewById(R.id.createAccountButton);
+        createAccountButton.setOnClickListener(this);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(usernameText.getText().length()!= 0 && passwordText.getText().length()!= 0 ) {
+                    loginButton.setClickable(true);
+                } else {
+                    loginButton.setClickable(false);
+                }
+            }
+            @Override public void afterTextChanged(Editable s) {}
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        };
+        usernameText.addTextChangedListener(textWatcher);
+        passwordText.addTextChangedListener(textWatcher);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.loginButton:
+                ParseUser.logInInBackground(usernameText.getText().toString(),
+                        passwordText.getText().toString(), new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Toast t = Toast.makeText(getApplicationContext(), "success!", Toast.LENGTH_SHORT);
+                            t.show();
+                            //TODO: go to next screen
+                        } else {
+                            // Signup failed. Look at the ParseException to see what happened.
+                        }
+                    }
+                });
+                break;
+            case R.id.createAccountButton:
+                Intent i = new Intent(this, CreateAccountActivity.class);
+                i.putExtra("username", usernameText.getText().toString());
+                startActivity(i);
+                break;
+        }
+    }
+}
